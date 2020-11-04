e5e2b9962827db787dc5128074aaeef6a614f8d6
Benoît Verhaeghe <badetitou@gmail.com>
2020-11-04 09:39:18 +0100

    Create .imageConf.sh

---
+++ .imageConf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ev
+
+# Give the name of your project (without spaces). The result files will look like this: Pharo8.0-64bit-MyProject.zip
+export PROJECT_NAME='Moose'
+
+# Comment on the versions you don't want to release.
+#PHARO[0]=70
+PHARO[1]=80
+PHARO[2]=90
+
+# Set true or false for which architecture you want
+export ARCH_32=false
+export ARCH_64=true
+
+# Put the Pharo script that will be executed when creating your Pharo images
+export PHARO_SCRIPT="Iceberg enableMetacelloIntegration: false.
+Metacello new
+	baseline: 'Moose';
+	repository: 'github://moosetechnology/Moose';
+	load.	
+Iceberg enableMetacelloIntegration: true.
+Smalltalk saveSession."
